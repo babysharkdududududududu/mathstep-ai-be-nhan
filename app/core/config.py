b94bc45169333aa556015e1777de0441adb9081f
Chi Thanh <thanh89508@gmail.com>
2026-04-11 14:08:47 +0700

    fix env

--- app/core/config.py
+++ app/core/config.py
@@ -18,6 +18,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 30
+    
 
     # Google OAuth
     GOOGLE_CLIENT_ID: str
